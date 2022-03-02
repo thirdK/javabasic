3b6d2962953d6487eeaf066ad2579a2f3db6dc3d
thirdK <dla0625@gmail.com>
2022-03-02 13:32:54 +0900

    a

---
+++ day01/src/pm/Exam1.java
@@ -0,0 +1,31 @@
+package pm;
+
+//클래스 선언부
+//public - 접근지정자
+//class Test - Test라는 class
+public class Exam1 { 
+	
+	//main 메서드
+	//public - 접근지정자
+	//static - 정적메소드 객체 필요없음
+	//void - return 없음
+	//main - 자바는 반드시 1개의 main 메서드가 있어야함
+	//String[] args - 문자열 배열 형태의 인수
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+		System.out.println("Hello World !");
+		
+		System.out.println("화면" + "출력");
+		System.out.println(3.14);
+		System.out.println(3+5);
+		System.out.println("화면" + 3);
+		System.out.println("화면" + 3 + 5);
+		System.out.println( 3 + 5 + "화면");
+		System.out.println("Hello !");
+		
+		int a = 3;
+		String b = "화면";
+		
+	}
+}
---
+++ day01/src/pm/Exam2.java
@@ -0,0 +1,12 @@
+package pm;
+
+public class Exam2 {
+// 1개의 소스파일 .java 에는 1개의 public class만 존재
+	
+}
+class Test00{
+	
+}
+class Test000{
+	
+}
